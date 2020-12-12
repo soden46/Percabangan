b494d6b95e0ddf2343885cad4e531a32db5e70c0
SYARIF <SYARIF@SYARIF-PC>
2020-12-12 20:52:49 +0700

    Project Enkapsulasi

---
+++ src/percabangan/Percabangan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percabangan;
+
+/**
+ *
+ * @author SYARIF
+ */
+public class Percabangan {
+
+    static int x =0;
+    public static void main(String[] args) {
+       int y=9;
+        if (y==10) {
+            System.out.println("sepuluh");   
+        }else{
+            System.out.println("bukan sepuluh");
+        }
+    }
+    
+}
